de9f63c22a532b41234d07e7cb96eadecc8551fc
adnanAbdulRahim <ad684424@dal.ca>
2024-09-23 20:20:50 -0300

    Updated according to rubric.

--- labs/lab2/js/script.js
+++ labs/lab2/js/script.js
@@ -4,6 +4,7 @@ console.log("Hello World!")
 function areaOfCircle(radius){
     //What is the scope of a const variable?
     //const has scope within the block and cannot be re-assigned;
+    //consts have to be given a value when declared.
     const pie = 3.14;
     var area = pie*radius**2;
     return area;
@@ -11,17 +12,26 @@ function areaOfCircle(radius){
 console.log(areaOfCircle(6));
 
 //b.
-function skibidiMath(a, b){
-    var results = {
-        addition: a + b,
-        subtraction: a - b,
-        multiplication: a * b,
-        division: b === 0 ?"Cannot divide by 0": a / b
-    }
-    return results;
+function mathAdd(a, b){
+    var sum = a + b;
+    return sum; 
 }
-console.log(skibidiMath(6,2));
-console.log(skibidiMath(6,0));
+function mathSubtract(a, b){
+    var sum = a - b;
+    return sum; 
+}
+function mathMultiply(a, b){
+    var sum = a * b;
+    return sum; 
+}
+function mathDivide(a, b){
+    return (b === 0) ? "Cannot divide 0" : a /b;
+}
+console.log(mathAdd(6, 2));
+console.log(mathSubtract(6, 2));
+console.log(mathMultiply(6, 2));
+console.log(mathDivide(6, 2));
+console.log(mathDivide(6, 0));
 
 //c.
 function areaOfRectangle(length, width){
@@ -31,5 +41,7 @@ function areaOfRectangle(length, width){
 }
 console.log(areaOfRectangle(20, 15));
 //When were the let and const keywords added to JavaScript?
-//2015
-//Source:https://www.w3schools.com/js/js_let.asp#:~:text=Before%20ES6%20(2015)%2C%20JavaScript,JavaScript%20keywords%3A%20let%20and%20const%20.
+//ES6(2015)
+//Sources:
+//https://www.w3schools.com/js/js_let.asp#:~:text=Before%20ES6%20(2015)%2C%20JavaScript,JavaScript%20keywords%3A%20let%20and%20const%20.
+//https://www.w3schools.com/js/js_const.asp
